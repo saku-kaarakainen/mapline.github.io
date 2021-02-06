ad7bb283085dbd242c9f901e516b6030c635d6c5
Saku Kaarakainen <sakuba91@hotmail.com>
2021-02-06 09:34:47 +0200

    fetch data from backend (but not really yet)

---
+++ src/ClientApp/src/models/Language.ts
@@ -0,0 +1,10 @@
+export class Language {        
+      public Id?: bigint;
+      public StringIdentifier?: string;
+      public Area?: JSON; 
+      public StartDate?: Date; 
+      public EndDate?: Date; 
+      public Features?: JSON; 
+      public AdditionalDetails?: JSON; 
+  }
+  
--- src/ClientApp/src/views/Map.vue
+++ src/ClientApp/src/views/Map.vue
@@ -14,9 +14,10 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { latLng } from "leaflet";
 import { LMap, LTileLayer } from "vue2-leaflet";
+import { Language } from '../models/Language';
 
 export default {
   name: "VMap",
@@ -26,15 +27,32 @@ export default {
   },
   data() {
     return {
+      loading: true,
+      showError: false,
+      errorMessage: 'An unexpected error occured while loading the information to the map.',
+      languages: [] as Language[],
       zoom: 3,
       center: latLng(60.166458996639314, 24.952770253219168),
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       mapOptions: {
-      }
+      }      
     };
   },
   methods: {
+    async fetchLanguages() {
+      try {
+        const response = await this.$axios.get<Language>[]('api/Map');
+        this.languages = response.data;
+      } catch (e) {
+        this.showError = true;
+        this.errorMessage = `An unexpected error occured while loading the information to the map. The error message: ${e.message}`;
+      }
+      this.loading = false;
+    },
+    async created() {
+      await this.fetchLanguages();
+    }
   }
 };
 </script>
