ce42ed044fed5e34d6e2f318936cfb6f40904533
Saku Kaarakainen <sakuba91@hotmail.com>
2021-02-09 07:31:45 +0200

    renamed the empty template class

---
+++ src/Mapline.Tests/Migrations/InitialMigrationTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// https://docs.microsoft.com/en-us/ef/core/testing/
+namespace Mapline.Tests.Migrations
+{
+    [TestClass]
+    public class InitialMigrationTests
+    {
+        [TestMethod]
+        public void Test_Up()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Down()
+        {
+        }
+    }
+}
