99ae555ad8f70381eb3db15930e57371ec818906
Saku Kaarakainen <sakuba91@hotmail.com>
2021-02-09 08:08:06 +0200

    created migrations tests

---
+++ src/Mapline.Tests/TestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Mapline.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mapline.Tests
+{
+    public class TestDbContext : MaplineDbContext
+    {
+        public TestDbContext(DbContextOptions<MaplineDbContext> options)
+             : base(options)
+        {
+            
+        }
+    }
+}
