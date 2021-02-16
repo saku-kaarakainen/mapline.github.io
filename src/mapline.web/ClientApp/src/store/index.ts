87e3842bafdf5eecd80c115a8a37c30cac81f8b9
Saku Kaarakainen <sakuba91@hotmail.com>
2021-02-16 09:29:57 +0200

    fixed a bug, now the store is working

--- src/mapline.web/ClientApp/src/store/index.ts
+++ src/mapline.web/ClientApp/src/store/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex, { StoreOptions } from 'vuex'
 import { RootState } from './types'
 import { counter } from './counter/index'
-import { editor } from '/editor/index'
+import { editor } from './editor/index'
 
 Vue.use(Vuex)
 
