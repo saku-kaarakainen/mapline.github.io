2c53b6bf7256f75e080430428987455eee976c77
Saku Kaarakainen <sakuba91@hotmail.com>
2021-02-17 16:33:44 +0200

    converted MapControlPlayer Javascript into TypeScript

--- src/mapline.web/ClientApp/src/components/MapControlPlayer.vue
+++ src/mapline.web/ClientApp/src/components/MapControlPlayer.vue
@@ -16,11 +16,11 @@
   <v-container id="map-control-player-template">
     <v-row class="row-1">
 
-      <v-text-field class="ma-2" :label="resources.yearHeader" v-model="local.currentYear" />
+      <v-text-field class="ma-2" :label="resources.yearHeader" v-model="currentYear" />
 
       <div class="map-control-player-buttons">
         <v-btn class="ma-2" @click="playOrPause">
-          <div class="playing" v-if="local.isPlaying">
+          <div class="playing" v-if="isPlaying">
             <v-icon left>mdi-pause</v-icon>
             Pause
           </div>
@@ -31,106 +31,117 @@
         </v-btn>
 
         <v-btn class="ma-2 map-control-player-button" @click="revertDirection">
-          <v-icon v-if="local.isDirectionToRight">mdi-arrow-right</v-icon>
+          <v-icon v-if="isDirectionToRight">mdi-arrow-right</v-icon>
           <v-icon v-else>mdi-arrow-left</v-icon>
         </v-btn>
       </div>
 
-      <v-text-field class="ma-2" :label="resources.intervalHeader" v-model="local.yearsInterval" />
+      <v-text-field class="ma-2" :label="resources.intervalHeader" v-model="yearsInterval" />
 
-      <v-text-field class="ma-2" :label="resources.updateRateHeader" v-model="local.updateRateInMilliseconds" />
+      <v-text-field class="ma-2" :label="resources.updateRateHeader" v-model="updateRateInMilliseconds" />
 
     </v-row>
 
     <v-row class="row-2" md="1">
       <v-slider id="ranged-slider"
-                v-model="local.currentYear"
-                :min="local.minYear"
-                :max="local.maxYear" />
+                v-model="currentYear"
+                :min="minYear"
+                :max="maxYear" />
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
+  import { Action, Getter, Mutation } from 'vuex-class'
+  import { Component, Vue, Prop } from 'vue-property-decorator'
 
-  export default {
-    name: 'map-control-player',
-    components: {
+  class PlayerResources {
+    yearHeader: string
+    intervalHeader: string
+    updateRateHeader: string
+  }
 
-    },
-
-    props: {
-      currentYear: Number,
-      minYear: Number,
-      maxYear: Number,
-
-      updateRateInMilliseconds: {
-        type: Number,
-        default: 1000 // 1 second
-      },
-
-      yearsInterval: {
-        type: Number,
-        default: 1 // years
-      }
-    },
-
-    data() {
-      return {
-        // local -object
-        // https://forum.vuejs.org/t/naming-practices-for-private-getter-variables/13905
-
-        resources: {
-          yearHeader: "Current Year:",
-          intervalHeader: "Interval (years):",
-          updateRateHeader: "Updates every (ms):"
-        },
-
-        local: {
-          isPlaying: true,
-          isDirectionToRight: true,
-          timer: '',
-
-          // These should be matched with props: { }
-          currentYear: this.currentYear,
-          minYear: this.minYear,
-          maxYear: this.maxYear,
-          updateRateInMilliseconds: this.updateRateInMilliseconds,
-          yearsInterval: this.yearsInterval
-        }
-      };
-    },
-
+  @Component
+  export default class MapControlPlayer extends Vue {
     async created() {
       try {
-        this.local.timer = setInterval(this.updateTimer, this.local.updateRateInMilliseconds);
+        this.timer = setInterval(this.updateTimer, this.updateRateInMilliseconds);
       } catch (e) {
         let message = `An unexpected error occuurred in components/MapControlPlayer.vue/async created.`;
         alert(message);
         console.log(`${message} The error:`);
         console.log(e);
       }
-    },
+    }
 
-    methods: {
-      updateTimer: function () {
-        if (this.local.isPlaying) {
 
-          if (this.local.isDirectionToRight)
-            this.local.currentYear += this.local.yearsInterval;
-          else
-            this.local.currentYear -= this.local.yearsInterval;
-        }
+    constructor() {
+      super()
 
-        this.$emit("update-year", this.local.currentYear);
-      },
-      cancelAutoUpdate() { clearInterval(this.local.timer); },
-      playOrPause: function () { this.local.isPlaying = !this.local.isPlaying; },
-      revertDirection: function () { this.local.isDirectionToRight = !this.local.isDirectionToRight; }
-    },
+      this.resources = new PlayerResources();
+      this.resources.yearHeader = "Current Year:",
+      this.resources.intervalHeader = "Interval (years):",
+      this.resources.updateRateHeader = "Updates every (ms):"
 
-    beforeDestroy() {
-      clearInterval(this.local.timer);
+      this.isPlaying = true
+      this.isDirectionToRight = true
+      this.timer = undefined
+    }
+
+    readonly resources: PlayerResources
+
+    @Prop() public currentYear: number
+    @Prop() public minYear: number
+    @Prop() public maxYear: number
+    @Prop({ default: 1000 }) public updateRateInMilliseconds: number
+    @Prop({ default: 1 }) public yearsInterval: number
+
+    private isPlaying: boolean
+    private isDirectionToRight: boolean
+    private timer: number
+
+    private updateTimer(): void {
+      if (this.isPlaying) {
+        if (this.isDirectionToRight)
+          this.currentYear += this.yearsInterval;
+        else
+          this.currentYear -= this.yearsInterval;
+      }
+
+      this.$emit("update-year", this.currentYear);
+    }
+
+    private cancelAutoUpdate(): void {
+      clearInterval(this.timer);
+    }
+
+    private playOrPause(): void {
+      this.isPlaying = !this.isPlaying;
+    }
+
+    private revertDirection(): void {
+      this.isDirectionToRight = !this.isDirectionToRight;
+    }
+
+    beforeDestroy(): void {
+      clearInterval(this.timer);
     }
   }
+
+  //export default {
+  //  name: 'map-control-player',
+  //  components: {
+  //  }
+  //  
+  //  
+  //  
+  //  
+  //  
+  //  
+  //  
+  //  
+  //  
+  //  
+
+  //}
 </script>
--- src/mapline.web/ClientApp/src/views/Show.vue
+++ src/mapline.web/ClientApp/src/views/Show.vue
@@ -5,157 +5,147 @@
   Component style coding / using the `scoped` attribute
 -->
 <style scoped>
-    .show-map-container {
-        height: 90%;
-        width: 100%;
-    }
+  .show-map-container {
+    height: 90%;
+    width: 100%;
+  }
 
-    .filter-bar-component {
-        display: inline-block;
-        width: 8%;
-        height: 100%;
-        vertical-align: top;
-    }
+  .filter-bar-component {
+    display: inline-block;
+    width: 8%;
+    height: 100%;
+    vertical-align: top;
+  }
 
-    .l-map {
-        width: 90%;
-        height: 100%;
-        top: 0px;
-        display: inline-block;
-    }
+  .l-map {
+    width: 90%;
+    height: 100%;
+    top: 0px;
+    display: inline-block;
+  }
 </style>
 
 
 <template>
-    <div class="show-map-container">
-        <map-control-player class="slider-component"
-                     v-bind:currentYear="-7000"
-                     @update-year="updateYear"
-                     v-bind:minYear=-7000
-                     v-bind:maxYear=2021
-                     v-bind:updateRateInMilliseconds="1000"
-                     v-bind:yearsInterval="5"
-                     />
+  <div class="show-map-container">
+    <map-control-player class="slider-component"
+                        v-bind:currentYear="-7000"
+                        @update-year="updateYear"
+                        v-bind:minYear=-7000
+                        v-bind:maxYear=2021
+                        v-bind:updateRateInMilliseconds="1000"
+                        v-bind:yearsInterval="5" />
 
-        <v-divider class="divider"></v-divider>
+    <v-divider class="divider"></v-divider>
 
-        <filter-bar class="filter-bar-component"
-                    @filters-changed="filtersChange"
-                    />
+    <filter-bar class="filter-bar-component"
+                @filters-changed="filtersChange" />
 
-        <v-divider class="divider" vertical></v-divider>
+    <v-divider class="divider" vertical></v-divider>
 
-        <p v-if="loading">Loading...</p>
-        <l-map class="l-map"
-               :zoom="zoom"
-               :center="center"
-               :options="mapOptions">
-            <l-tile-layer :url="url"
-                          :attribution="attribution" />
+    <p v-if="loading">Loading...</p>
+    <l-map class="l-map"
+           :zoom="zoom"
+           :center="center"
+           :options="mapOptions">
+      <l-tile-layer :url="url"
+                    :attribution="attribution" />
 
-            <l-geo-json :geojson="languagesGeoJson"
-                        :options="options"
-                        :options-style="styleFunction" />
-        </l-map>
-    </div>
+      <l-geo-json :geojson="languagesGeoJson"
+                  :options="options"
+                  :options-style="styleFunction" />
+    </l-map>
+  </div>
 </template>
 
 <script lang="ts">
-    import { latLng } from "leaflet";
-    import { LMap, LTileLayer, LGeoJson } from "vue2-leaflet";
-    import { Language } from '../models/Language';
-    import FilterBar from '@/components/FilterBar.vue' // @ is an alias to /src
-    import MapControlPlayer from '@/components/MapControlPlayer.vue'
+  import L from 'leaflet-draw'
+  import { latLng } from "leaflet";
+  import { LMap, LTileLayer, LGeoJson } from "vue2-leaflet";
+  import { Language } from '../models/Language';
+  import FilterBar from '@/components/FilterBar.vue' // @ is an alias to /src
+  import MapControlPlayer from '@/components/MapControlPlayer.vue'
 
-    export default {
-        name: "VMap",
-        components: {
-            LMap,
-            LTileLayer,
-            LGeoJson,
-            FilterBar,
-            MapControlPlayer
-        },
-        data() {
-            return {
-                loading: true,
-                show: true,
-                languagesGeoJson: null,
-                errorMessage: 'An unexpected error occured while loading the information to the map.',
-                languages: [] as Language[],
-                zoom: 3,
-                center: latLng(60.166458996639314, 24.952770253219168),
-                url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-                mapOptions: {},
-                fillColor: "#e4ce7f", // blue
-                marker: latLng(47.41322, -1.219482)
-            };
-        },
+  export default {
+    name: "VMap",
+    components: {
+      LMap,
+      LTileLayer,
+      LGeoJson,
+      FilterBar,
+      MapControlPlayer
+    },
+    data() {
+      return {
+        loading: true,
+        show: true,
+        languagesGeoJson: null,
+        errorMessage: 'An unexpected error occured while loading the information to the map.',
+        languages: [] as Language[],
+        zoom: 3,
+        center: latLng(60.166458996639314, 24.952770253219168),
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        mapOptions: {},
+        fillColor: "#e4ce7f", // blue
+        marker: latLng(47.41322, -1.219482)
+      };
+    },
 
-        methods: {
-            updateYear(year) {
-                //console.log(`the year is: ${year}`)
-            },
+    methods: {
+      updateYear(year) {
+        if (year > -1000) {
+          window.map.removeLayer(this.languagesGeoJson);
+        }
+      }
+    },
 
-            filtersChange(filters) {
-                //console.log(`the filters are`);
-                //console.log(filters);
-            }
-        },
+    computed: {
+      options() {
+        return { onEachFeature: this.onEachFeatureFucntion };
+      },
 
-        computed: {
-            options() {
-                return { onEachFeature: this.onEachFeatureFucntion };
-            },
+      styleFunction() {
+        const fillColor = this.fillColor; // important! need touch fillColor in computed for re-calculate when change fillColor
+        return () => {
+          return {
+            weight: 2,
+            color: "#ECEFF1", // Orange
+            opacity: 1,
+            fillColor: fillColor,
+            fillOpacity: 1
+          };
+        };
+      },
 
-            styleFunction() {
-                const fillColor = this.fillColor; // important! need touch fillColor in computed for re-calculate when change fillColor
-                return () => {
-                    return {
-                        weight: 2,
-                        color: "#ECEFF1", // Orange
-                        opacity: 1,
-                        fillColor: fillColor,
-                        fillOpacity: 1
-                    };
-                };
-            },
+      onEachFeatureFunction() {
+        if (!this.enableTooltip) {
+          // return () => { };
+          return;
+        }
 
-            onEachFeatureFunction() {
-                if (!this.enableTooltip) {
-                    // return () => { };
-                    return;
-                }
-
-                return (feature, layer) => {
-                    layer.bindTooltip(`<div>code:${feature.properties.code}</div>`
-                        + `<div>nom: ${feature.properties.nom}</div>`,
-                        { permanent: false, sticky: true }
-                    );
-                };
-            },
+        return (feature, layer) => {
+          layer.bindTooltip(`<div>code:${feature.properties.code}</div>`
+            + `<div>nom: ${feature.properties.nom}</div>`,
+            { permanent: false, sticky: true }
+          );
+        };
+      },
 
 
-        },
+    },
 
-        async created() {
-            try {
-                this.loading = true;
-                const response = await this.$axios.get<Language[]>('api/map/languages')
-
-                const data = await response.data;
-
-                console.log("async create. data received:");
-                console.log(data);
-
-                this.languagesGeoJson = data;
-                this.loading = false;
-            } catch (e) {
-                alert("An unexpected error occured in API handling...");
-                console.log("An unexpected error occured in API handling. The error:");
-                console.log(e);
-            }
-        },
-    };
+    async created() {
+      try {
+        this.loading = true;
+        const response = await this.$axios.get<Language[]>('api/map/languages');
+        this.languagesGeoJson = await response.data;
+        this.loading = false;
+      } catch (e) {
+        alert("An unexpected error occured in API handling...");
+        console.log("An unexpected error occured in API handling. The error:");
+        console.log(e);
+      }
+    },
+  };
 </script>
