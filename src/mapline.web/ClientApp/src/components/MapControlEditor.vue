ca99e95d4574df707a16f590731f4a9d1548125e
Saku Kaarakainen <sakuba91@hotmail.com>
2021-02-20 14:45:07 +0200

    Revert "redesigned MapControlEditor..."
    This reverts commit d2ae440343c62c7a8dee64c4918fc0e6275a15cf.

--- src/mapline.web/ClientApp/src/components/MapControlEditor.vue
+++ src/mapline.web/ClientApp/src/components/MapControlEditor.vue
@@ -21,9 +21,9 @@
 
     <v-row class="row-1">
       <div class="col-md-1">
-        <v-btn type="submit" @click.prevent="saveToDb">{{ resources.addToDb }}</v-btn>
+        <v-btn type="submit" @click.prevent="saveToDb">Add to DB</v-btn>
         <br />
-        <v-btn type="submit" @click.prevent="saveToFile">{{ resources.addToFile }}</v-btn>
+        <v-btn type="submit" @click.prevent="saveToFile">Add to File</v-btn>
       </div>
       <div class="col-md-11">
         <v-text-field class="ma-1" :label="resources.name" v-model="currentLanguage.name" />
@@ -44,110 +44,109 @@
 
 
 <script lang="ts">
-  import { mapGetters, mapMutations } from 'vuex'
   import { Action, Getter, Mutation } from 'vuex-class'
   import { Component, Vue, Prop } from 'vue-property-decorator'
   import { Language } from '@/store/editor/types'
-  import store from '@/store'
   import { SaveLanguage } from '../models/SaveLanguage'
-
   const namespace = 'editor'
 
-  export default {
-    name: "map-control-editor",
+  // TODO: An actual resource editor
+  class EditorResorces {
+    name: string
+    yearStart: string
+    yearEnd: string
+  }
 
-    components: {},
+  @Component
+  export default class Editor extends Vue {
+    constructor() {
+      super()
+      this.resources = new EditorResorces();
+      this.resources.name = "Name";
+      this.resources.yearStart = "Start Year";
+      this.resources.yearEnd = "End Year";
 
-    props: {
-      scaleMin: { type: Number, default: -10000 },
-      scaleMax: { type: Number, default: 2021 }
-    },
+      this.loading = false; 
+    }
 
-    data() {
-      return {
-        loading: false,
-        resources: {
-          name: "Name",
-          yearStart: "Start Year",
-          yearEnd: "End Year",
-          addToDb: "Add to DB",
-          addToFile: "Add to file"
-        }
+    readonly resources: EditorResorces
+    private loading: boolean  
+
+    @Prop({ default: -10000 })
+    public scaleMin: number
+
+    @Prop({ default: 2021 })
+    public scaleMax: number
+
+    @Getter('currentLanguage', { namespace })
+    private currentLanguage!: Language    
+
+    @Mutation('updateEditorData', { namespace })
+    private updateEditorData!: (value: Language) => void
+
+    public update(): void {
+      this.updateEditorData(this.currentLanguage);
+    }
+
+    public async saveToDb(): Promise<void> {
+      try {
+        this.loading = true;
+        await this.save('api/administrator/save/db');
+        this.loading = false;
+
+        alert("Data added to the database");
+      } catch (e) {
+        var message = "An error occurred while adding the map data.";
+        alert(message);
+        console.log(`${message} The error:`);
+        console.log(e);
       }
-    },
+    }
 
-    computed: {
-      ...mapGetters({ currentLanguage: 'editor/currentLanguage' }),
-      ...mapMutations({ updateEditorData: 'editor/updateEditorData' }),
-    },
+    public async saveToFile(): Promise<void> {
+      try {
+        this.loading = true;
+        await this.save('api/administrator/save/file');
+        this.loading = false;
 
-    methods: {
+        alert("Data added to the file");
+      } catch (e) {
+        var message = "An error occurred while adding the map data.";
+        alert(message);
+        console.log(`${message} The error:`);
+        console.log(e);
+      }
+    }
 
-      update(): void {
-        this.updateEditorData(this.local_currentLanguage);
-      },
+    public async save(url: string): Promise<void> {
+      this.throwIfInvalid(); 
 
-      async saveToDb() {
-        try {
-          this.loading = true;
-          await this.save('api/administrator/save/db');
-          this.loading = false;
-
-          alert("Data added to the database");
-        } catch (e) {
-          var message = "An error occurred while adding the map data.";
-          alert(message);
-          console.log(`${message} The error:`);
-          console.log(e);
-        }
-      },
-
-      async saveToFile(): Promise<void> {
-        try {
-          this.loading = true;
-          await this.save('api/administrator/save/file');
-          this.loading = false;
-
-          alert("Data added to the file");
-        } catch (e) {
-          var message = "An error occurred while adding the map data.";
-          alert(message);
-          console.log(`${message} The error:`);
-          console.log(e);
-        }
-      },
-
-      async save(url: string): Promise<void> {
-        this.throwIfInvalid();
-
-        console.log("save/this.local_currentLanguage");
-        var language = {
-          Name: this.local_currentLanguage_name,
-          GeoJson: this.local_currentLanguage_area,
-          YearStart: this.local_currentLanguage_yearStart,
-          YearEnd: this.local_currentLanguage_yearEnd,
-        };
+      var language = {
+        Name: this.currentLanguage.name,
+        GeoJson: this.currentLanguage.area,
+        YearStart: this.currentLanguage.yearRange[0],
+        YearEnd: this.currentLanguage.yearRange[1],
+      };
 
         await this.$axios.post<SaveLanguage>(url, language)
-      },
+    }
 
-      throwIfInvalid(): boolean {
-        // name
-        if (!this.local_currentLanguage_name)
-          throw "Validation failed: invalid name";
-
-        // area
-        if (!this.local_currentLanguage_area)
-          throw "Validation failed: invalid area";
-
-        // year range
-        //if (!this.local_currentLanguage_yearRange
-        //  || this.local_currentLanguage.yearRange.length != 2
-        //  || this.local_currentLanguage.yearRange[0] === undefined
-        //  || this.local_currentLanguage.yearRange[1] === undefined)
-        //  throw "Validation failed: invalid years";
-
-      }
+    private throwIfInvalid(): boolean {
+      // name
+      if (!this.currentLanguage.name) 
+        throw "Validation failed: invalid name";
+      
+      // area
+      if (!this.currentLanguage.area) 
+        throw "Validation failed: invalid area";
+      
+      // year range
+      if (!this.currentLanguage.yearRange
+        || this.currentLanguage.yearRange.length != 2
+        || this.currentLanguage.yearRange[0] === undefined
+        || this.currentLanguage.yearRange[1] === undefined) 
+        throw "Validation failed: invalid years";
+      
     }
   }
 </script>
