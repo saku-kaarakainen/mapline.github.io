82388b4ba10b70285a95a6b3380076f075ca5a1b
Saku Kaarakainen <sakuba91@hotmail.com>
2021-02-23 21:15:12 +0200

    continued migration script

--- src/mapline.migrations/InitialMigration.cs
+++ src/mapline.migrations/InitialMigration.cs
@@ -17,15 +17,58 @@ namespace Mapline.Migrations
 
         protected override void Up([NotNull] MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable<object>(
-    name: "LanguageFilter",
-    columns: table => throw new NotImplementedException(),
-    // long LanguageId   
-    // Language Language 
-    // long FilterId     
-    // Filter Filter     
-    constraints: table => throw new NotImplementedException()
-);
+            migrationBuilder.CreateTable(
+                name: "Language",
+                columns: table => new
+                {
+                    Id = table.PK_Column<long>(),
+                    Name = table.Column<string>(nullable: false),
+                    Area = table.Column<Geometry>(nullable: false),
+                    YearCurrent = table.Column<int>(nullable: true),
+                    YearStart = table.Column<int>(nullable: true),
+                    YearEnd = table.Column<int>(nullable: true),
+                    Features = table.Column<string>(nullable: true), // JSON
+                    AdditionalDetails = table.Column<string>(nullable: true), // JSON
+                },
+                constraints: table => table.PrimaryKey("PK_Language_Id", x => x.Id));
+
+            migrationBuilder.CreateTable(
+                name: "Filter",
+                columns: table => new
+                {
+                    Id = table.PK_Column<long>(),
+                    Name = table.Column<string>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filter_Id", x => x.Id);
+                    table.PrimaryKey("PK_Filter_Name", f => f.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LanguageFilter",
+                columns: table => new
+                {
+                    Id = table.PK_Column<long>(),
+                    LanguageId = table.Column<long>(),
+                    FilterId = table.Column<long>()
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LanguageFilter_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LanguageFilter_Language_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Language",
+                        principalColumn: "LanguageId",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_LanguageFilter_Filter_FilterId",
+                        column: x => x.FilterId,
+                        principalTable: "Filter",
+                        principalColumn: "FilterId",
+                        onDelete: ReferentialAction.SetNull);
+                });
 
             migrationBuilder.CreateTable(
                 name: "LanguageRelationship",
@@ -34,7 +77,7 @@ namespace Mapline.Migrations
                     Id = table.PK_Column<long>(),
                     Type = table.Column<int>(),
                     ParentId = table.Column<long>(),
-                    ChildID = table.Column<long>(),
+                    ChildId = table.Column<long>(),
                 },
                 constraints: table =>
                 { 
@@ -45,56 +88,13 @@ namespace Mapline.Migrations
                         principalTable: "Language",
                         principalColumn: "ParentId",
                         onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_LanguageRelationship_Language_ChildId",
+                        column: x => x.ChildId,
+                        principalTable: "Language",
+                        principalColumn: "ChildId",
+                        onDelete: ReferentialAction.SetNull);
                 });           
-
-            migrationBuilder.CreateTable(
-                name: "Language",
-                columns: table => new 
-                {
-                    Id = table.PK_Column<long>(),
-                    LanguageFilterId = table.Column<long>(nullable: true),
-                    Name = table.Column<string>(nullable: false),
-                    Area = table.Column<Geometry>(nullable: false),
-                    YearCurrent = table.Column<int>(nullable: true),
-                    YearStart = table.Column<int>(nullable: true),
-                    YearEnd = table.Column<int>(nullable: true),
-                    Features = table.Column<string>(nullable: true), // JSON
-                    AdditionalDetails = table.Column<string>(nullable: true), // JSON
-
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Language_Id", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Language_LanguageFilter_LanguageFilterId",
-                        column: x => x.LanguageFilterId,
-                        principalTable: "LanguageFilter",
-                        principalColumn: "LanguageFilterId",
-                        onDelete: ReferentialAction.SetNull);
-                }
-            );
-
-            migrationBuilder.CreateTable(
-                name: "Filter",
-                columns: table => new
-                {
-                    Id = table.PK_Column<long>(),
-                    LanguageFilterId = table.Column<long>(nullable: true),
-                    Name = table.Column<string>(nullable: false),
-                },
-                constraints: table => 
-                {
-                    table.PrimaryKey("PK_Filter_Id", x => x.Id);
-                    table.PrimaryKey("PK_Filter_Name", f => f.Name);
-                    table.ForeignKey(
-                        name: "FK_Filter_LanguageFilter_LanguageFilterId",
-                        column: x => x.LanguageFilterId,
-                        principalTable: "LanguageFilter",
-                        principalColumn: "LanguageFilterId",
-                        onDelete: ReferentialAction.SetNull);
-
-                }
-            );
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
