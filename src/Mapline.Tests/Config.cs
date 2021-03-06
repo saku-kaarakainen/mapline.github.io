2e2e397b688093f2fa4291cd2e9e7d259c4dfde6
Saku Kaarakainen <sakuba91@hotmail.com>
2021-03-06 16:57:03 +0200

    improved code

--- src/Mapline.Tests/Config.cs
+++ src/Mapline.Tests/Config.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace Mapline.Tests
 {
+    /// <summary>
+    /// Configuration class for the unit tests.
+    /// </summary>
     public class Config 
     {
+        /// <summary>
+        /// Creates the IConfiguration from the "appsettings.json".
+        /// </summary>
+        /// <returns></returns>
         public static IConfiguration GetAppJson()
         {
             return new ConfigurationBuilder()
--- src/Mapline.Tests/Helpers/InMemoryContextFactory.cs
+++ src/Mapline.Tests/Helpers/InMemoryContextFactory.cs
@@ -10,9 +10,13 @@ using System.Threading.Tasks;
 
 namespace Mapline.Tests.Helpers
 {
+    /// <summary>
+    /// Database context factory class that saves data in-memory for the unit tests.
+    /// </summary>
     public class InMemoryContextFactory : IDbContextFactory<MaplineDbContext>
     {
         private readonly IDataBuilder dataBuilder;
+        private readonly DbContextOptions<MaplineDbContext> options;
 
         private static IDataBuilder CreateDefaultDataBuilder()
         {
@@ -20,24 +24,34 @@ namespace Mapline.Tests.Helpers
             return LanguagesBuilder.CreateDataBuilder(config);
         }
 
+        /// <summary>
+        /// Initializes the default instance of the class.
+        /// </summary>
         public InMemoryContextFactory() 
             : this(CreateDefaultDataBuilder()) 
         { 
 
         }
 
+        /// <summary>
+        /// Initializes the instance with the specified data builder.
+        /// </summary>
+        /// <param name="dataBuilder">The data builder.s</param>
         public InMemoryContextFactory(IDataBuilder dataBuilder)
         {
             this.dataBuilder = dataBuilder ?? throw new ArgumentNullException(nameof(dataBuilder));
-        }
-
-        public MaplineDbContext CreateDbContext()
-        {
-            var options = new DbContextOptionsBuilder<MaplineDbContext>()
+            this.options = new DbContextOptionsBuilder<MaplineDbContext>()
                  .UseInMemoryDatabase("Mapline")
                  .Options;
+        }
 
-            var context = new MaplineDbContext(options, this.dataBuilder);
+        /// <summary>
+        /// Creates the database context.
+        /// </summary>
+        /// <returns></returns>
+        public MaplineDbContext CreateDbContext()
+        {
+            var context = new MaplineDbContext(this.options, this.dataBuilder);
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
